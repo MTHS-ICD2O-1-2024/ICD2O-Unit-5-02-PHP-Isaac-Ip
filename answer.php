f365376e5fcdc854ae6482a4d4d40beafd053d14
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-04-23 17:18:07 -0400

    added improvements

--- answer.php
+++ answer.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Guess a Random Number, in PHP" />
+  <meta name="description" content="Positive and Negative #, in PHP" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Isaac Ip" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -14,7 +14,7 @@
   <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
   <link rel="manifest" href="./site.webmanifest" />
-  <title>Guess a Random Number, in PHP</title>
+  <title>Positive and Negative #, in PHP</title>
 </head>
 
 <body>
@@ -23,12 +23,12 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Guess a Random Number, in PHP</span>
+        <span class="mdl-layout-title">Positive and Negative #, in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/dice.gif" alt="dice" width="250" />
+        <img src="./images/positive_negative.jpg" alt="positive_negative" width="250" />
       </div>
       <div class="page-content-php">
         <div id="user-info">
